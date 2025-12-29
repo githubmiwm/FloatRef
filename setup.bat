d666642c66eea9cfa37e3323eb41e8e301beed16
githubmiwm <82645444+githubmiwm@users.noreply.github.com>
2025-12-29 15:28:07 +0900

    Add files via upload

--- setup.bat
+++ setup.bat
@@ -21,13 +21,13 @@ if %errorlevel% equ 0 (
 
 :: 3. まだ入っていない場合：インストールを実行
 echo [OK] Pythonを確認しました。
-echo 必要なライブラリ(PyQt6)が見つからないため、インストールします...
+echo 必要なライブラリ PyQt6 が見つからないため、インストールします...
 echo --------------------------------------------------------
 python -m pip install PyQt6
 echo --------------------------------------------------------
 echo.
 echo [成功] 準備が整いました！
-echo この画面を閉じて、アプリ(float_ref.pyw)を起動してください。
+echo この画面を閉じて、アプリ float_ref.pyw を起動してください。
 echo.
 pause
 exit /b
@@ -35,10 +35,10 @@ exit /b
 :: ----------------------------------------------------------
 
 :AlreadyInstalled
-echo [OK] 必要な環境(Python と PyQt6)はすでにインストールされています。
+echo [OK] 必要な環境 Python と PyQt6 はすでにインストールされています。
 echo.
 echo 何もする必要はありません。
-echo そのままアプリ(float_ref.pyw)を起動してください。
+echo そのままアプリ float_ref.pyw を起動してください。
 echo.
 pause
 exit /b
@@ -46,7 +46,7 @@ exit /b
 :InstallPython
 echo [!] Pythonが見つかりませんでした。
 echo.
-echo このアプリを動かすには「Python」が必要です。
+echo このアプリを動かすには Python が必要です。
 echo Enterキーを押すと、自動的にインストール画面(Microsoft Store)が開きます。
 echo.
 pause > nul
@@ -57,7 +57,7 @@ echo  【重要：インストール後の手順】
 echo.
 echo  1. 開いた画面で「入手」または「インストール」を押してください。
 echo  2. インストールが完了したら、この黒い画面を閉じてください。
-echo  3. もう一度、この「setup.bat」をダブルクリックしてください。
+echo  3. もう一度、この setup.bat をダブルクリックしてください。
 echo ========================================================
 echo.
 pause
